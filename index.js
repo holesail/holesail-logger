85da61c82a95a1e1fc1d6334f64c5972bb11dffa
Raven Scott <snxraven@gmail.com>
2025-08-01 06:23:50 +0000

    Convert to use barely-colours

--- index.js
+++ index.js
@@ -1,56 +1,50 @@
+const colors = require('barely-colours');
+
 class HolesailLogger {
   constructor ({ prefix = 'Holesail', enabled = false, level = 1 } = {}) {
-    this.prefix = prefix
-    this.enabled = enabled
-    this.minLevel = level
+    this.prefix = prefix;
+    this.enabled = enabled;
+    this.minLevel = level;
     this.LOG_LEVELS = {
       DEBUG: 0,
       INFO: 1,
       WARN: 2,
       ERROR: 3
-    }
-    this.colors = {
-      reset: '\x1b[0m',
-      gray: '\x1b[90m',
-      blue: '\x1b[34m',
-      green: '\x1b[32m',
-      yellow: '\x1b[33m',
-      red: '\x1b[31m'
-    }
+    };
   }
 
   log ({ type, msg }) {
-    if (!this.enabled || type < this.minLevel) return
-    let levelStr, color, consoleMethod
+    if (!this.enabled || type < this.minLevel) return;
+    let levelStr, colorFunc, consoleMethod;
     switch (type) {
       case this.LOG_LEVELS.DEBUG:
-        levelStr = 'DEBUG'
-        color = this.colors.gray
-        consoleMethod = console.log
-        break
+        levelStr = 'DEBUG';
+        colorFunc = colors.brightBlack;
+        consoleMethod = console.log;
+        break;
       case this.LOG_LEVELS.INFO:
-        levelStr = 'INFO'
-        color = this.colors.green
-        consoleMethod = console.log
-        break
+        levelStr = 'INFO';
+        colorFunc = colors.green;
+        consoleMethod = console.log;
+        break;
       case this.LOG_LEVELS.WARN:
-        levelStr = 'WARN'
-        color = this.colors.yellow
-        consoleMethod = console.warn
-        break
+        levelStr = 'WARN';
+        colorFunc = colors.yellow;
+        consoleMethod = console.warn;
+        break;
       case this.LOG_LEVELS.ERROR:
-        levelStr = 'ERROR'
-        color = this.colors.red
-        consoleMethod = console.error
-        break
+        levelStr = 'ERROR';
+        colorFunc = colors.red;
+        consoleMethod = console.error;
+        break;
       default:
-        return
+        return;
     }
-    const timestamp = `${this.colors.gray}${new Date().toISOString()}${this.colors.reset}`
-    const prefix = `${this.colors.blue}[${this.prefix}]${this.colors.reset}`
-    const level = `${color}[${levelStr}]${this.colors.reset}`
-    consoleMethod(`${timestamp} ${prefix} ${level} ${msg}`)
+    const timestamp = colors.brightBlack(new Date().toISOString());
+    const prefix = colors.blue(`[${this.prefix}]`);
+    const level = colorFunc(`[${levelStr}]`);
+    consoleMethod(`${timestamp} ${prefix} ${level} ${msg}`);
   }
 }
 
-module.exports = HolesailLogger
+module.exports = HolesailLogger;
